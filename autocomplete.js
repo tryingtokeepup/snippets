891fac1b844cd2ae3f3c98d20ddbbfe87925ef46
tryingtokeepup <kl.hawaii@gmail.com>
2019-06-16 13:52:22 -0700

    first commit, trying new flow

---
+++ autocomplete.js
@@ -0,0 +1,37 @@
+class MyInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+  }
+
+  render() {
+    return (
+      <ReactAutocomplete
+        items={[
+          { id: 'foo', label: 'foo' },
+          { id: 'bar', label: 'bar' },
+          { id: 'baz', label: 'baz' }
+        ]}
+        shouldItemRender={(item, value) =>
+          item.label.toLowerCase().indexOf(value.toLowerCase()) > -1
+        }
+        getItemValue={item => item.label}
+        renderItem={(item, highlighted) => (
+          <div
+            key={item.id}
+            style={{ backgroundColor: highlighted ? '#eee' : 'transparent' }}
+          >
+            {item.label}
+          </div>
+        )}
+        value={this.state.value}
+        onChange={e => this.setState({ value: e.target.value })}
+        onSelect={value => this.setState({ value })}
+      />
+    );
+  }
+}
+
+ReactDOM.render(<MyInput />, document.body);
